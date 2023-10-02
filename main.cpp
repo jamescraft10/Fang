d6f8d181bc4889c424cf6138058fc1ebcce50836
JamesCraft10 <69867626+James251133@users.noreply.github.com>
2023-10-01 21:51:47 -0400

    Add Project

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+bool CheckIfExtension(string::size_type n, string const& s, int Length) {
+    if (string::npos == n) {
+        cout << "\nThis file or folder does not contain \".fa\"";
+        return 1;
+    } else {
+        if(Length - 2 == n) {
+            return 0;
+        } else {
+            cout << "\nThis file or folder does not contain \".fa\"";
+            return 1;
+        }
+    }
+}
+
+int main(int argc, char* argv[]) {
+    string FilePath = argv[1];
+    if(CheckIfExtension(FilePath.find(".fa"), FilePath, FilePath.length())) {
+        return 1;
+    }
+
+    string FileContents;
+    string OneLine;
+    ifstream Program(FilePath);
+    while(getline(Program, OneLine)) {
+        FileContents += OneLine;
+    }
+    Program.close();
+
+    cout << FileContents << endl;
+    return 0;
+}
