af5894ff792285c0662b9e211714a0a8af7a041d
JamesCraft10 <69867626+James251133@users.noreply.github.com>
2023-10-08 21:00:39 -0400

    Hello, World!

--- main.cpp
+++ main.cpp
@@ -41,6 +41,19 @@ int main(int argc, char* argv[]) {
     std::string Output = "";
     for(int i = 0; i <= FileContents.length(); ++i) {
         // Return
+        if(FileContents[i] == 'p' && FileContents[i+1] == 'r' && FileContents[i+2] == 'i' && FileContents[i+3] == 'n' && FileContents[i+4] == 't' && FileContents[i+5] == ' ') {
+            Output += "printf(\"";
+            int j = 6;
+            while(true) {
+                ++j;
+                if(FileContents[i+j] != '"') {
+                    Output += FileContents[i+j];
+                } else if(FileContents[i+j] == '"') {
+                    break;
+                }
+            }
+            Output += "\");\n";
+        }
         if(FileContents[i] == 'r' && FileContents[i+1] == 'e' && FileContents[i+2] == 't' && FileContents[i+3] == 'u' && FileContents[i+4] == 'r' && FileContents[i+5] == 'n' && FileContents[i+6] == ' ') {
             Output += "return ";
             int j = 6;
@@ -55,6 +68,13 @@ int main(int argc, char* argv[]) {
             Output += ";";
         }
     }
+
+    std::string Name = FilePath;
+    Name[Name.length() - 1] = ' ';
+    Name[Name.length() - 2] = 'c';
+    std::ofstream OutputFile(Name);
+    OutputFile << "#include <stdio.h>\nint main() {\n" << Output << "\n}";
+    OutputFile.close();
 
     std::cout << Output << std::endl;
 
