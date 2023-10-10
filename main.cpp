886ea30dc26c6a14f959ea19dcf161d79cdd5cbb
JamesCraft10 <69867626+James251133@users.noreply.github.com>
2023-10-10 18:21:05 -0400

    Better Code

--- main.cpp
+++ main.cpp
@@ -1,79 +1,63 @@
 #include <iostream>
-#include <iomanip>
 #include <fstream>
 #include <string>
 
-bool CheckIfExtension(std::string::size_type n, std::string const& s, int Length) {
-    if(std::string::npos == n) {
-        std::cout << "\nThis file does not contain \".fa\"";
-        return 1;
-    } else {
-        if(Length - 3 == n) {
-            return 0;
+std::string extractContents(const std::string& fileContents) {
+    std::string output;
+    size_t i = 0;
+    while (i < fileContents.size()) {
+        if (fileContents.substr(i, 6) == "print ") {
+            i += 6;
+            size_t startQuote = fileContents.find('"', i);
+            size_t endQuote = fileContents.find('"', startQuote + 1);
+            if (startQuote != std::string::npos && endQuote != std::string::npos) {
+                std::string content = fileContents.substr(startQuote + 1, endQuote - startQuote - 1);
+                output += "printf(\"" + content + "\");\n";
+                i = endQuote + 1;
+            }
+        } else if (fileContents.substr(i, 7) == "return ") {
+            output += "return ";
+            i += 7;
+            while (i < fileContents.size() && fileContents[i] != ';') {
+                output += fileContents[i++];
+            }
+            output += ";\n";
         } else {
-            std::cout << "\nThis file does not contain \".fa\"";
-            return 1;
+            ++i;
         }
     }
+    return output;
 }
 
 int main(int argc, char* argv[]) {
-    if(argv[1] == 0) {
-        std::cout << "Must pass in a file" << std::endl;
+    if (argc != 2) {
+        std::cout << "Usage: " << argv[0] << " <filename>\n";
         return EXIT_FAILURE;
     }
 
     std::string FilePath = argv[1];
-    if(CheckIfExtension(FilePath.find(".fa"), FilePath, FilePath.length())) {
+    std::ifstream inputFile(FilePath);
+    if (!inputFile.is_open()) {
+        std::cerr << "Error: Could not open input file.\n";
         return EXIT_FAILURE;
     }
 
-    std::string FileContents;
-    std::string OneLine;
-    std::ifstream Program(FilePath);
-    while(getline(Program, OneLine)) {
-        FileContents += OneLine;
-    }
-    Program.close();
+    std::string FileContents((std::istreambuf_iterator<char>(inputFile)), std::istreambuf_iterator<char>());
+    inputFile.close();
 
-    std::string Output = "";
-    for(int i = 0; i <= FileContents.length(); ++i) {
-        // Return
-        if(FileContents[i] == 'p' && FileContents[i+1] == 'r' && FileContents[i+2] == 'i' && FileContents[i+3] == 'n' && FileContents[i+4] == 't' && FileContents[i+5] == ' ') {
-            Output += "printf(\"";
-            int j = 6;
-            while(true) {
-                ++j;
-                if(FileContents[i+j] != '"') {
-                    Output += FileContents[i+j];
-                } else if(FileContents[i+j] == '"') {
-                    break;
-                }
-            }
-            Output += "\");\n";
-        }
-        if(FileContents[i] == 'r' && FileContents[i+1] == 'e' && FileContents[i+2] == 't' && FileContents[i+3] == 'u' && FileContents[i+4] == 'r' && FileContents[i+5] == 'n' && FileContents[i+6] == ' ') {
-            Output += "return ";
-            int j = 6;
-            while(true) {
-                ++j;
-                if(FileContents[i+j] != ';') {
-                    Output += FileContents[i+j];
-                } else if(FileContents[i+j] == ';') {
-                    break;
-                }
-            }
-            Output += ";";
-        }
+    std::string Output = extractContents(FileContents);
+
+    std::string OutputFileName = FilePath.substr(0, FilePath.find_last_of('.')) + ".c";
+    std::ofstream OutputFile(OutputFileName);
+    if (!OutputFile.is_open()) {
+        std::cerr << "Error: Could not create output file.\n";
+        return EXIT_FAILURE;
     }
 
-    std::string Name = FilePath;
-    Name[Name.length() - 1] = ' ';
-    Name[Name.length() - 2] = 'c';
-    std::ofstream OutputFile(Name);
-    OutputFile << "#include <stdio.h>\nint main() {\n" << Output << "\n}";
+    OutputFile << "#include <stdio.h>\nint main() {\n" << Output << "\nreturn 0;\n}";
     OutputFile.close();
-    std::cout << Output << std::endl;
+
+    std::cout << "C program generated successfully.\n";
 
     return EXIT_SUCCESS;
 }
