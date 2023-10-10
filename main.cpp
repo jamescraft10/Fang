bb82e01e6f40bc44d5a52ab217ab8605a6c337f4
JamesCraft10 <69867626+James251133@users.noreply.github.com>
2023-10-10 18:32:31 -0400

    Better

--- main.cpp
+++ main.cpp
@@ -1,43 +1,46 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <string>
 
 std::string extractContents(const std::string& fileContents) {
     std::string output;
-    size_t i = 0;
-    while (i < fileContents.size()) {
-        if (fileContents.substr(i, 6) == "print ") {
-            i += 6;
-            size_t startQuote = fileContents.find('"', i);
-            size_t endQuote = fileContents.find('"', startQuote + 1);
-            if (startQuote != std::string::npos && endQuote != std::string::npos) {
-                std::string content = fileContents.substr(startQuote + 1, endQuote - startQuote - 1);
-                output += "printf(\"" + content + "\");\n";
-                i = endQuote + 1;
+    std::string line;
+    std::istringstream stream(fileContents);
+    while(std::getline(stream, line)) {
+        // Check if the line ends with a semicolon
+        if(!line.empty() && line.back() == ';') {
+            size_t pos = line.find("print ");
+            if(pos != std::string::npos) {
+                pos += 6;
+                size_t startQuote = line.find('"', pos);
+                size_t endQuote = line.find('"', startQuote + 1);
+                if(startQuote != std::string::npos && endQuote != std::string::npos) {
+                    std::string content = line.substr(startQuote + 1, endQuote - startQuote - 1);
+                    output += "printf(\"" + content + "\");\n";
+                }
+            } else if(line.find("return ") == 0) {
+                size_t pos = line.find("return ") + 7;
+                output += "return " + line.substr(pos, line.size() - pos - 1) + ";\n";
             }
-        } else if (fileContents.substr(i, 7) == "return ") {
-            output += "return ";
-            i += 7;
-            while (i < fileContents.size() && fileContents[i] != ';') {
-                output += fileContents[i++];
-            }
-            output += ";\n";
         } else {
-            ++i;
+            // Handle error: Missing semicolon at the end of the statement
+            std::cerr << "Error: Missing semicolon at the end of the statement: " << line << "\n";
+            return "";
         }
     }
     return output;
 }
 
 int main(int argc, char* argv[]) {
-    if (argc != 2) {
+    if(argc != 2) {
         std::cout << "Usage: " << argv[0] << " <filename>\n";
         return EXIT_FAILURE;
     }
 
     std::string FilePath = argv[1];
     std::ifstream inputFile(FilePath);
-    if (!inputFile.is_open()) {
+    if(!inputFile.is_open()) {
         std::cerr << "Error: Could not open input file.\n";
         return EXIT_FAILURE;
     }
@@ -46,10 +49,13 @@ int main(int argc, char* argv[]) {
     inputFile.close();
 
     std::string Output = extractContents(FileContents);
+    if(Output.substr(0, 4) == "ERROR") {
+        return EXIT_FAILURE;
+    }
 
     std::string OutputFileName = FilePath.substr(0, FilePath.find_last_of('.')) + ".c";
     std::ofstream OutputFile(OutputFileName);
-    if (!OutputFile.is_open()) {
+    if(!OutputFile.is_open()) {
         std::cerr << "Error: Could not create output file.\n";
         return EXIT_FAILURE;
     }
